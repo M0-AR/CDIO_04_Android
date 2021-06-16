982113a4142777c56484c4aa9a53accda361e134
Sid-Ali-Mounib <sidali-m@hotmail.com>
2021-06-16 16:13:07 +0200

    deleted if statemnt to get all cards in array

--- app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/DetectorActivity.java
@@ -202,11 +202,11 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
                                         Log.e("CHECK", "run: " + result.toString());
                                         Log.e("CHECK", "run: " + result.getTitle());
                                         Log.e("CHECK", "run: " + result.getConfidence());
-                                        if (result.getConfidence() > 0.40){
+
                                             cardName.add(result.getTitle());
                                             Log.e("CHECK", "X value: " + result.getLocation().centerX());
                                             Log.e("CHECK", "Y value: " + result.getLocation().centerY());
-                                        }
+
                                     }
                                 }
                                 Log.e("CHECK", cardName.toString());
