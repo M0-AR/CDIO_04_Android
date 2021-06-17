91010312d42d5c268f8a2bd55f5c578f5a2ecdc2
Sid-Ali-Mounib <sidali-m@hotmail.com>
2021-06-17 14:45:09 +0200

    fix

--- app/src/main/java/org/tensorflow/lite/examples/detection/CardLogic/CardGameInstance.java
+++ app/src/main/java/org/tensorflow/lite/examples/detection/CardLogic/CardGameInstance.java
@@ -11,47 +11,51 @@ import java.util.Set;
 
 public class CardGameInstance {
 
-    public String startGame(HashSet<String> cameraCards){
+    private final Pile[] tableauPile;
+    private final Pile[] foundationPile;
+    private CardSolver solver;
+
+    public CardGameInstance() {
+        tableauPile = new TableauPile[1];
+        foundationPile = new FoundationPile[4];
+        solver = new CardSolver();
+
+        for (int i = 0; i < tableauPile.length; i++) {
+            tableauPile[i] = new TableauPile();
+        }
+
+        for (int i = 0; i < foundationPile.length; i++) {
+            foundationPile[i] = new FoundationPile();
+        }
+
+    }
+
+    public String startGame(HashSet<String> cameraCards) {
 
         CardSolver Solver = new CardSolver();
 
         // TODO: 6/16/2021 Delete this later(find better solution)
         ArrayList<String> cameraCards_ = new ArrayList<>(cameraCards);
-        
+
         ArrayList<String> stringCards = new ArrayList<String>();
 
 
-
-
-        Pile[] piles = new TableauPile[7];
-        piles[6] = new TableauPile();
-
-
-
-
-
-        for (int i = 0; i < 6 ; i++) {
+        for (int i = 0; i < 6; i++) {
 
             String[] words = cameraCards_.get(i).split(" ");
             String suit = words[0].toUpperCase();
 
 
-            Card cardIndex = new Card(Suit.valueOf(suit),Rank.setRank(words[1]));
+            Card cardIndex = new Card(Suit.valueOf(suit), Rank.setRank(words[1]));
 
-            piles[i]=new TableauPile();
-            piles[i].addCard(cardIndex);
+            tableauPile[i].addCard(cardIndex);
 
 
         }
 
-       return Solver.solveGame(piles);
+        return Solver.solveGame(tableauPile, foundationPile);
     }
-
-
-
-
-    private int findValueOfCard(String card){
-        return Integer.parseInt(card);
-    }
-
 }
+
+
+
